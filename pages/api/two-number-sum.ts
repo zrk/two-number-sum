20819ea69f547ced4add94754c95e3a96dd2f826
al <48353128+zrk@users.noreply.github.com>
2020-08-17 01:08:31 +0300

    add /api/two-number-sum

---
+++ pages/api/two-number-sum.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { twoNumberSum } from '../../services/twoNumberSum';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { n, s },
+    // method,
+  } = req;
+
+  // console.log(array[2]);
+
+  const intArray = Array<string>()
+    .concat(n) // one or more `n`, make it array for sure
+    .map((str) => str?.split(',')) // for `?n=1,2,3`
+    .flat() // for `?n=1&n=2,3`
+    .map(Number)
+    .filter(Boolean); // remove non-numbers
+  const isValidArray = intArray?.length > 1
+
+  const sum = Number(s);
+
+  if ( !isValidArray || isNaN(sum)) {
+    res.status(400).end();
+    return;
+  }
+
+  const [first, second, ...other] = intArray;
+  const array = [first, second, ...other] as const;
+
+  res.statusCode = 200;
+  res.json(twoNumberSum({ array, sum }));
+
+  // res.json({
+  //   n,
+  //   array,
+  //   s,
+  //   sum,
+  //   method,
+  //   result: twoNumberSum({ array, sum }),
+  // });
+};
+
+export default handler;
